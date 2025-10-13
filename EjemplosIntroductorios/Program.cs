411ea703f10bb650697a2171d4dfd6b0bf32bd8f
rusok <rusok@RUSOKI7>
2025-10-13 19:20:47 -0400

    Agregar archivos de proyecto.

---
+++ EjemplosIntroductorios/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EjemplosIntroductorios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
