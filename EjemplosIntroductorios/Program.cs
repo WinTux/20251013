8915fd91965692eec5bbbafaff29adb02cd9e81c
rusok <rusok.software@gmail.com>
2025-10-14 19:56:41 -0400

    Arreglos y argumentos

--- EjemplosIntroductorios/Program.cs
+++ EjemplosIntroductorios/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("¡Hola mundo!");
+
+            // Tipos de datos
+            int numeroEntero = 42;
+            float numeroFlotante = 3.14f;
+            double numeroDecimal = 3.14;
+            string texto = "Hola, C#";
+            bool esVerdadero = true;
+            char caracter = 'c';
+            Console.WriteLine("Entero: "+numeroEntero+", Decimal: "+ numeroFlotante);
+            Console.WriteLine($"Entero: {numeroEntero}, Decimal: {numeroDecimal}, Texto: {texto}, Booleano: {esVerdadero}, Carácter: {caracter}");
         }
     }
 }
